241faad029cf4a6416c7cbec2810378a8b14cde6
Victor <231650261@alumno.uptex.edu.mx>
2025-12-01 23:23:28 -0600

    Inyeccion de componentes FXML

--- src/main/java/com/dulceria/pos/controlador/ProductosController.java
+++ src/main/java/com/dulceria/pos/controlador/ProductosController.java
@@ -14,4 +14,34 @@ import java.util.List;
 
 
 public class ProductosController {
+
+    //Inyeccion de componentes FXML
+
+    // Componentes del formulario
+    private @FXML TextField txtNombre;
+    private @FXML ComboBox<String> comboCategoria;
+    private @FXML ComboBox<String> comboMarca;
+    private @FXML TextField txtStock;
+    private @FXML ComboBox<String> comboUnidad;
+    private @FXML TextField txtPrecio;
+    private @FXML CheckBox checkActivo;
+    private @FXML Button btnGuardar;
+    private @FXML Button btnLimpiar;
+
+    //componentes de la tabla
+    private @FXML TableView<Producto> tablaProductos;
+    private @FXML TableColumn<Producto, Integer> colId;
+    private @FXML TableColumn<Producto, String> colNombre;
+    private @FXML TableColumn<Producto, String> colCategoria;
+    private @FXML TableColumn<Producto, String> colMarca;
+    private @FXML TableColumn<Producto, Double> colStock;
+    private @FXML TableColumn<Producto, String> colUnidad;
+    private @FXML TableColumn<Producto, Double> colPrecio;
+    private @FXML TableColumn<Producto, Boolean> colActivo;
+
+    //Componentes de busqueda
+    private @FXML TextField txtBuscar;
+    private @FXML Button btnBuscar;
+    private @FXML ComboBox<String> comboFiltrarCategoria;
+
 }
