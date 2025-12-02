51acf20b65866671fbc608a08b02c01767cc90eb
Victor <231650261@alumno.uptex.edu.mx>
2025-12-01 23:07:08 -0600

    importacion de todos los modulos necesarios para trabajar

--- src/main/java/com/dulceria/pos/controlador/ProductosController.java
+++ src/main/java/com/dulceria/pos/controlador/ProductosController.java
@@ -1,4 +1,17 @@
 package com.dulceria.pos.controlador;
 
+// Daos y modelos necesarios para manejar el controlador
+import com.dulceria.pos.DAO.ProductoDAO;
+import com.dulceria.pos.DAO.CategoriaDAO;
+import com.dulceria.pos.DAO.MarcaDAO;
+import com.dulceria.pos.modelo.Categoria;
+import com.dulceria.pos.modelo.Producto;
+import com.dulceria.pos.modelo.Marca;
+import javafx.fxml.FXML;
+
+import javafx.scene.control.*;
+import java.util.List;
+
+
 public class ProductosController {
 }
